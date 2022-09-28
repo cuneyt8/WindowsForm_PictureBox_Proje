12529f1ef6d841282de152f2433174d83dcf9be3
Cüneyt <100092812+cuneyt8@users.noreply.github.com>
2022-09-28 23:11:16 +0300

    Picturebox Kullanımı
    Picturebox içerisinde button ve fotoğrafların kullanımı

---
+++ PictureBox_Örnek/PictureBox_Örnek/PictureBox_Örnek/Form1.cs
@@ -0,0 +1,36 @@
+﻿using PictureBox_Örnek.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBox_Örnek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"C:\Users\cuney\OneDrive\Masaüstü\henry.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Resources.zidane;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "https://s.hs-data.com/bilder/spieler/gross/1159.jpg?fallback=png";
+        }
+    }
+}
